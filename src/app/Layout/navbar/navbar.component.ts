ab9e4c6541a4a4d1ec04b7138b76acd08dc86f7f
Ahmed Saad <ahmedsaadmedo505@gmail.com>
2025-03-05 20:52:13 +0200

    add home paga, navbar and footer

--- src/app/Layout/navbar/navbar.component.ts
+++ src/app/Layout/navbar/navbar.component.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  isLoggedIn: boolean = false;
+  userName: string = '';
+  userImage: string = '';
+  unreadNotifications: number = 0;
+  isNavbarHidden: boolean = false;
 
+  ngOnInit(): void {
+    this.checkUserStatus();
+  }
+
+  checkUserStatus() {
+    const userData = JSON.parse(localStorage.getItem('user') || '{}');
+
+    if (userData && userData.isLoggedIn) {
+      this.isLoggedIn = true;
+      this.userName = userData.name || 'مستخدم';
+      this.userImage = userData.image || 'assets/default-user.png';
+      this.unreadNotifications = userData.notifications || 0;
+    } else {
+      this.isLoggedIn = false;
+    }
+  }
+
+  login() {
+    console.log('توجه إلى صفحة تسجيل الدخول...');
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    this.isLoggedIn = false;
+    this.userName = '';
+    this.userImage = '';
+    this.unreadNotifications = 0;
+    console.log('تم تسجيل الخروج.');
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    if (window.scrollY > 50) {
+      this.isNavbarHidden = true;
+    } else {
+      this.isNavbarHidden = false;
+    }
+  }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./Layout/navbar/navbar.component";
+import { HomeComponent } from "./Pages/Home/home/home.component";
+import { FooterComponent } from "./Layout/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, HomeComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
