695bab92f52e702d88ef9aec4ecc16e3deeab5e0
Mayar-Fawzy <momayar920@gmail.com>
2025-03-05 10:22:37 +0200

    add ngx-toastr and bootstrap styles, configure routing, and update global styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,37 @@
 import { Routes } from '@angular/router';
+import { NotfoundComponent } from './Layout/notfound/notfound.component';
+import { AdminComponent } from './Pages/Admin/admin/admin.component';
+import { ForgetpasswordComponent } from './Pages/Auth/forgetpassword/forgetpassword.component';
+import { LoginComponent } from './Pages/Auth/login/login.component';
+import { RegisterComponent } from './Pages/Auth/register/register.component';
+import { CustomerComponent } from './Pages/Customer/customer/customer.component';
+import { DonorComponent } from './Pages/Donor/donor/donor.component';
+import { VolnteerComponent } from './Pages/volunteer/volnteer/volnteer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', redirectTo:'login', pathMatch:'full'},
+    {path:'login' , title:'login', component:LoginComponent},
+    {path:'register' , title:'register', component:RegisterComponent},
+    {path:'forgetpassword' , title:'forgetpassword', component:ForgetpasswordComponent},
+    {path:'admin',title:'admin',
+        component:AdminComponent,
+        children:[]
+    },
+    {path:'donor',title:'donor',
+        component:DonorComponent,
+        children:[]
+    },
+    {path:'customor',title:'customor',
+        component:CustomerComponent,
+        children:[]
+    },
+    {path:'volunteer',title:'volunteer',
+        component:VolnteerComponent,
+        children:[]
+    },
+    {
+        path: '**',
+        title: 'Not Found',
+        component:NotfoundComponent
+    }
+];
