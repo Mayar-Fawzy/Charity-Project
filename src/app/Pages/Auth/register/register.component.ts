02e19bb34bc7f51ef0a40b7e2fcf9adb2a30a760
Mayar-Fawzy <momayar920@gmail.com>
2025-03-11 22:35:01 +0200

    feat: enhance registration form validation with improved password requirements and error handling

--- src/app/Pages/Auth/register/register.component.ts
+++ src/app/Pages/Auth/register/register.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { RouterLink,Router } from '@angular/router';
-import { FormGroup, FormControl, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl,AbstractControl, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-register',
   standalone: true,
@@ -17,34 +17,28 @@ export class RegisterComponent {
   // constructor(private _AuthService:AuthService  ,private _router:Router){}
   registerForm:FormGroup=new FormGroup({
      
-    birthdate:new FormControl(null ,[Validators.required,    Validators.pattern(/^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/(19|20)\d{2}$/)]),
+    birthdate:new FormControl(null ,[Validators.required,    Validators.pattern(/^(0[1-9]|[12][0-9]|3[01])\/([1-9]|1[0-2])\/(19|20)\d{2}$/)]),
       email:new FormControl(null ,[Validators.required,Validators.email]),
-      password:new FormControl(null ,[Validators.required, Validators.pattern(/^[A-Z0-9a-z]{6,}/)]),
+      password:new FormControl(null ,[Validators.required, Validators.minLength(8), Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)]),
       name: new FormControl(null, [Validators.required, Validators.minLength(3)]),
-      rePassword:new FormControl(null ,[Validators.required, Validators.pattern(/^[A-Z0-9a-z]{6,}/)]),
+      rePassword:new FormControl(null ,[Validators.required,Validators.minLength(8), Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)]),
       gender:new FormControl(null ,[Validators.required]),
      
-     })
+     }, this.confirmPassword
+    
+    )
      SubmitRegister(forminfo:FormGroup)
      {
-  //     this.isloading=true;
-  //     this._AuthService.Register(forminfo.value).subscribe((response)=>{
-       
-  //     if(response.message === "success"){
-  //       //خزنت ال token
-  //       this.isloading=false;
-      
+  
           this._router.navigate(['/login']);
-  //       }
-  //     },
-  //   (error) => {
-  //     console.log(error);
-  //     this.isloading=false;
-  //     if (error.status === 409) {
-  //       alert("User already exists");
-  //     }
-  //     this.errorpassword=error.error.errors.msg;
-  //   }
-  // )}
+   
+     }
+     confirmPassword(g :AbstractControl){
+      if(g.get('password')?.value=== g.get('rePassword')?.value){
+        return null
+      }
+      else{
+        return {mismatch:true}
+      }
      }
 }
