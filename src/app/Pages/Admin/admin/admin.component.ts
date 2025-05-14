3942c067ac6394db13da777675bdbef909fcd576
Amed-Saad <ahmed.saad.bayoumi.farag@gmail.com>
2025-05-14 18:17:24 +0300

    dash

--- src/app/Pages/Admin/admin/admin.component.ts
+++ src/app/Pages/Admin/admin/admin.component.ts
@@ -1,7 +1,6 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { LoginService } from '../../Auth/core/Services/login.service';
 
 @Component({
   selector: 'app-admin',
@@ -11,22 +10,11 @@ import { LoginService } from '../../Auth/core/Services/login.service';
   styleUrls: ['./admin.component.scss']
 })
 export class AdminComponent {
-  private readonly _LoginService=inject(LoginService)
-  sidebarOpen = true;
-<<<<<<< HEAD
-  userData:any
-  userName!:string
-=======
+  sidebarOpen = false;
+  
   showItemDonationsSubmenu = false;
 
->>>>>>> 148fa6d (dashbord-inkinddonation)
   toggleSidebar() {
     this.sidebarOpen = !this.sidebarOpen;
   }
- ngOnInit(): void {
-   this.userData = this._LoginService.saveUserAuth();
-  
-this.userName=this.userData?.["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname"]+" " + this.userData?.["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname"]
- }
-  
 }
