2dda4fe95ee373896fd99157e4319c5d141b1d67
Mayar-Fawzy <mayarfawzy715@gmail.com>
2025-05-17 22:20:12 +0200

    feat(projects): add ProjectStatusArPipe for Arabic project status translation and update project status display

---
+++ src/app/Pages/Projects/Core/Pipe/project-status-ar.pipe.ts
@@ -0,0 +1,24 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'projectStatusAr',
+  standalone: true
+})
+export class ProjectStatusArPipe implements PipeTransform {
+
+   transform(status: any): string {
+    switch (status) {
+      case 1:
+      case 'Ongoing':
+        return 'جاري التنفيذ';
+      case 'Completed':
+        return 'مكتمل';
+      case 'Pending':
+        return 'بانتظار التنفيذ';
+      default:
+        return 'غير معروف';
+    
+    }
+  }
+
+}
