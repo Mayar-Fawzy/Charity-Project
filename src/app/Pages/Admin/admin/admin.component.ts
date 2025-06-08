8565b94aea11e2e2f1d960fde11540a2051002f6
Mayar-Fawzy <mayarfawzy715@gmail.com>
2025-06-09 01:45:25 +0200

    fix(admin): add signOut method and update styles for danger button

--- src/app/Pages/Admin/admin/admin.component.ts
+++ src/app/Pages/Admin/admin/admin.component.ts
@@ -23,5 +23,7 @@ export class AdminComponent {
   
 this.userName=this.userData?.["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname"]+" " + this.userData?.["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname"]
  }
-  
+  signOut() {
+    this._LoginService.signOut();
+  }
 }
