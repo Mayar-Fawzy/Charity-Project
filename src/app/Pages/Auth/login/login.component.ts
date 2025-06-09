476a2709f3bc2348beae7e48f719add49af73d7d
Mayar-Fawzy <mayarfawzy715@gmail.com>
2025-06-09 22:48:01 +0200

    loginEdit

--- src/app/Pages/Auth/login/login.component.ts
+++ src/app/Pages/Auth/login/login.component.ts
@@ -97,7 +97,15 @@ export class LoginComponent {
       (error) => {
         this.isloading = false;
         console.log('error', error);
-        this._ToastService.error(error.error.errors, 'error');
+        if( error.error.statusCode == 403) {
+          this._ToastService.error('يجب عليك تاكيد البريد الالكتروني قبل تسجيل الدخول', 'خطأ');  
+        }
+        else if (error.error.statusCode == 400) {
+          this._ToastService.error('البريد الالكتروني او كلمة المرور غير صحيحة', 'خطأ');
+        }
+        else {
+        this._ToastService.error(error.error.errors, 'خطأ');
+      }
       }
     );
   }
